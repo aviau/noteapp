619354d2fd22ab793e2f1aef9a5544826a65354d
Alexandre Viau <alexandre@alexandreviau.net>
2022-07-03 19:17:18 -0400

    simplify main.ts (#16)

--- src/main/application/noteApplication.ts
+++ src/main/application/noteApplication.ts
@@ -1,18 +1,29 @@
-import { IpcMainConnection, IpcMainConnectionEvent } from './ipcMainConnection';
-import { IpcChannel, IpcChannelMessage } from '../common/ipc';
-import { ConfigurationService } from './configuration/configurationService';
+import {
+  IpcMainConnection,
+  IpcMainConnectionEvent,
+} from '../ipcMainConnection';
+import { IpcChannel, IpcChannelMessage } from '../../common/ipc';
+import { ConfigurationService } from '../configuration/configurationService';
+import { initApp } from './startup/startup';
 
 /*
  * NoteApplication contains all of the application's state and logic.
  */
 export class NoteApplication {
+  electronApp: Electron.App;
+
   configurationService: ConfigurationService;
 
   constructor(electronApp: Electron.App, ipcMainConnection: IpcMainConnection) {
+    this.electronApp = electronApp;
     this.configurationService = new ConfigurationService(
       electronApp.getPath('userData')
     );
     ipcMainConnection.onPing((e, m) => this.onPing(e, m));
+  }
+
+  start(): void {
+    initApp(this.electronApp);
   }
 
   private async onPing(
---
+++ src/main/application/startup/appUpdater.ts
@@ -0,0 +1,10 @@
+import { autoUpdater } from 'electron-updater';
+import log from 'electron-log';
+
+export class AppUpdater {
+  constructor() {
+    log.transports.file.level = 'info';
+    autoUpdater.logger = log;
+    autoUpdater.checkForUpdatesAndNotify();
+  }
+}

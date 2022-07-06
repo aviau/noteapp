02f59d61e570d84774cb3a9d113b2aeef0fc8473
Sophie Bernadin-Mercier <sophiebmercier@hotmail.com>
2022-07-06 16:22:00 -0400

    Set default theme colors for light and dark mode

--- src/ui/foundation/components/ThemeProvider/theme.ts
+++ src/ui/foundation/components/ThemeProvider/theme.ts
@@ -1,22 +1,44 @@
 import { createTheme } from '@mui/material/styles';
-import { red } from '@mui/material/colors';
+
+const lightTheme = {
+  primary: {
+    main: '#8273e6',
+  },
+  secondary: {
+    main: '#f2f3f5',
+    dark: '#e3e5e8',
+  },
+  text: {
+    primary: '#2e3338',
+    secondary: '#3b4045',
+  },
+  background: {
+    default: '#f2f3f5',
+  },
+};
+
+const darkTheme = {
+  primary: {
+    main: '#4d3ca6',
+  },
+  secondary: {
+    main: '#161616',
+    dark: '#000000',
+  },
+  text: {
+    primary: '#dcddde',
+    secondary: '#8e8e8e',
+  },
+  background: {
+    default: '#161616',
+  },
+};
 
 const theme = (mode: 'light' | 'dark') => {
   return createTheme({
-    zIndex: {
-      appBar: 1251,
-    },
     palette: {
       mode,
-      primary: {
-        main: '#556cd6',
-      },
-      secondary: {
-        main: '#19857b',
-      },
-      error: {
-        main: red.A400,
-      },
+      ...(mode === 'light' ? lightTheme : darkTheme),
     },
   });
 };
